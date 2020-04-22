37e65cba10775e822d211cf0e1d45d3ebe28064b
Onesinus SPT <onesinus231@gmail.com>
2020-04-23 02:15:24 +0700

    Add toast incompatible device

--- screens/AbsenceScreen.js
+++ screens/AbsenceScreen.js
@@ -2,16 +2,28 @@ import React, { useState, useEffect } from 'react';
 import { Image } from 'react-native';
 import { 
   Container, Content, Button, Text, Grid, Col,
-  Card, CardItem, Thumbnail, Left, Body, Right
+  Card, CardItem, Thumbnail, Left, Body, Right, Toast, Root
 } from 'native-base';
 
 // import * as Location from 'expo-location';
+import * as LocalAuthentication from 'expo-local-authentication';
 import { Camera } from "../components";
 
 export default function AbsenceScreen() {
   // const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [imgFace, setImgFace] = useState(null);
+
+  const onCheckIn = async () => {
+    const isHasHardware = await LocalAuthentication.hasHardwareAsync();
+    // if (!isHasHardware) {
+      Toast.show({
+        text: 'Device is not supported for face or fingerprint',
+        buttonText: 'OK',
+        duration: 5000
+      });
+    // }
+  }
 
   // useEffect(() => {
   //   const getLocation = async () => {
@@ -58,44 +70,53 @@ export default function AbsenceScreen() {
     return <Camera setImgFace={setImgFace} />
   }else {
     return (
-        <Container>
-          <Content>
-            <Card>
-              <CardItem>
-                <Left>
-                  <Thumbnail source={{uri: ''}} />
-                  <Body>
-                    <Text>Onesinus Saut Parulian</Text>
-                    <Text note>22 April 2020 17:59</Text>
-                    <Text note>Rusun Griya Tipar Cakung</Text>
-                  </Body>
-                </Left>
-              </CardItem>
-              <CardItem>
-                <Right>
+      <Root>
+        
+          <Container>
+            <Content>
+              <Card>
+                <CardItem>
+                  <Left>
+                    <Thumbnail source={{uri: ''}} />
                     <Body>
-                      <Image source={{uri: imgFace}} style={{height: 200, width: 200, flex: 1}}/>
+                      <Text>Onesinus Saut Parulian</Text>
+                      <Text note>22 April 2020 17:59</Text>
+                      <Text note>Rusun Griya Tipar Cakung</Text>
                     </Body>
-                  </Right>
+                  </Left>
                 </CardItem>
-            </Card>
-            <Grid>
-              <Col>
-                <Button full success><Text> Re-Take Location</Text></Button>
-              </Col>
-              <Col>
-                <Button 
-                  full 
-                  info
-                  onPress={() => setImgFace(null)}
-                  >
-                    <Text> Re-Take Picture</Text>
-                </Button>
-              </Col>
-            </Grid>
-            <Button full primary><Text> Check In </Text></Button>
-          </Content>
-      </Container>
+                <CardItem>
+                  <Right>
+                      <Body>
+                        <Image source={{uri: imgFace}} style={{height: 200, width: 200, flex: 1}}/>
+                      </Body>
+                    </Right>
+                  </CardItem>
+              </Card>
+              <Grid>
+                <Col>
+                  <Button full success><Text> Re-Take Location</Text></Button>
+                </Col>
+                <Col>
+                  <Button 
+                    full 
+                    info
+                    onPress={() => setImgFace(null)}
+                    >
+                      <Text> Re-Take Picture</Text>
+                  </Button>
+                </Col>
+              </Grid>
+              <Button 
+                full 
+                primary
+                onPress={onCheckIn}
+              >
+                <Text> Check In </Text>
+              </Button>
+            </Content>
+        </Container>
+      </Root>
     );
   }
 }
